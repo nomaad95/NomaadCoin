89cc3be296494526e1468ce3c616517eca2fd249
Camille <camille.bamboute@hotmail.fr>
2020-12-15 21:27:24 +0100

    first commit

---
+++ blockchain.h
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <vector>
+#include "block.h"
+using namespace std;
+
+
+class Blockchain {
+  public:
+    Blockchain();
+    void AddBlock(Block bNew);
+  private:
+    uint32_t _nDifficulty;
+    vector<Block> _vChain;
+    Block _GetLastBlock() const;
+};

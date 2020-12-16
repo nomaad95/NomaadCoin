7a8ff7ed1ef8c87f36146cbd381011ff595855bf
Camille <camille.bamboute@hotmail.fr>
2020-12-16 18:43:23 +0100

    changing main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include"blockchain.h"
 
 int main() {
-Blockchain bChain = Blockchain();
-cout << "Mining block 1…" << endl;
-bChain.AddBlock(Block(1, "Block 1 Data"));
-cout << "Mining block 2…" << endl;
-bChain.AddBlock(Block(2, "Block 2 Data"));
-cout << "Mining block 3…" << endl;
-bChain.AddBlock(Block(3, "Block 3 Data"));
-return 0;
+  Blockchain bChain = Blockchain();
+  cout << "Mining block 1…" << endl;
+  bChain.AddBlock(Block(1, "Block 1 Data"));
+  cout << "Mining block 2…" << endl;
+  bChain.AddBlock(Block(2, "Block 2 Data"));
+  cout << "Mining block 3…" << endl;
+  bChain.AddBlock(Block(3, "Block 3 Data"));
+  return 0;
 }

8d08d775b92033d00c63de110afc4637fe6fa9e1
Camille <camille.bamboute@hotmail.fr>
2020-12-30 17:56:26 +0100

    setting constuctor

--- transaction.cpp
+++ transaction.cpp
@@ -1,6 +1,8 @@
 #include"header/transaction.h"
 #include<sstream>
 
-Transaction::Transaction(string fromAddress, string toAddress, int amount){
-
+Transaction::Transaction(string from, string to, int am){
+	fromAddress = from;
+	toAddress = to;
+	amount = am;
 }

a45a5ee2f5a60381d0a98e0fae9f661fb585e642
Camille <camille.bamboute@hotmail.fr>
2020-12-30 15:30:05 +0100

    changing block calls and creating root block

--- blockchain.cpp
+++ blockchain.cpp
@@ -6,7 +6,9 @@ Blockchain::Blockchain() {
   time_t now = time(0);
   char* dt = ctime(&now);
 	list<Transaction> trans;
-_vChain.emplace_back(Block(time(0), trans));
+	Block root = Block(time(0), trans, _vChain.size());
+	root.sPrevHash = root._CalculateHash();
+_vChain.emplace_back(root);
 _nDifficulty = 5;
 }
 
@@ -17,20 +19,43 @@ _vChain.push_back(bNew);
 }
 
 void Blockchain::minePendingTransactions(string miningRewardAddress){
-
-	Block block = Block(time(0), pendingTransactions);
+	cout << "number of blocks" << pendingTransactions.size() << endl;
+	Block block = Block(time(0), pendingTransactions, _vChain.size());
+	block.sPrevHash = _GetLastBlock().GetHash();
 	block.MineBlock(_nDifficulty);
 	_vChain.emplace_back(block);
 	pendingTransactions.empty();
-	Transaction t1 =  Transaction(NULL, miningRewardAddress, miningRewards );
+	Transaction t1 =  Transaction("", miningRewardAddress, miningRewards );
 	pendingTransactions.push_back(t1);
   cout << "new block mined, giving reward" << endl;
 
 
 }
 
+double Blockchain::getBalance(string walletAddress){
+	double balance = 0;
+	vector<Block>::iterator blockIterator;
+	list<Transaction>::iterator transIterator;
+	for(blockIterator = _vChain.begin(); blockIterator != _vChain.end(); blockIterator++ ){
+		for(transIterator = blockIterator->sDataIn.begin(); transIterator != blockIterator->sDataIn.end(); transIterator++){
+			if(transIterator->fromAddress == walletAddress){
+				balance -= transIterator->amount;
+				cout << "went in" <<endl;
+			}
+
+			if(transIterator->toAddress == walletAddress){
+				balance += transIterator->amount;
+				cout << "went in" <<endl;
+
+			}
+		}
+	}
+	return balance;
+}
+
 void Blockchain::createTransaction(Transaction transaction){
 	pendingTransactions.push_back(transaction);
+	cout << "pending transactions :" << pendingTransactions.size() << endl;
 }
 
 bool Blockchain::isChainValid(){

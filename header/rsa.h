bb479b023c21bbb8770b032d52aaf683344c7e2d
Camille <camille.bamboute@hotmail.fr>
2021-02-17 12:27:55 +0100

    adding methods in rsa header

--- header/rsa.h
+++ header/rsa.h
@@ -6,8 +6,17 @@
 using namespace std;
 
 
-class KeyGeneration{
+class Rsa{
 public:
+	Rsa(int * pub, int * pri, int * mod);
+
+private:
+	int randomBigPrime( int max);
+	mp::cpp_int testBigPrime(mp::cpp_int a);
+	mp::cpp_int recursiveBigGCD(mp::cpp_int a, mp::cpp_int b, mp::cpp_int *x, mp::cpp_int *y);
+	int recursiveGCD(int a, int b, int *x, int *y)
+
+
 
 
 };
